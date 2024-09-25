cf2e9a38b448acfe9d1dd227f9a4334122e29d11
Rıfat Rahvalı <rifatrahvali@outlook.com>
2024-09-25 15:09:17 +0300

    controller syntaxını değiştirdim

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('/anasayfa',[HomeController::class,'index']);
+
+// v3 Service Provider ile değiştirdik
+Route::get('/anasayfa','HomeController@index');
