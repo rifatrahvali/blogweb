6207d51c411a681e26767576b7f0391dc70583e5
Rıfat Rahvalı <rifatrahvali@outlook.com>
2024-09-26 21:18:37 +0300

    bolum 25 - model - factory - seeder kullandık.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             // 
-            CategorySeeder::class
+            CategorySeeder::class,
+            ArticleSeeder::class
         ]);
     }
 }
